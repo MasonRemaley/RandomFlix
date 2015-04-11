84acbb5c3f89648ac54137bc02aa1e068e76ed7a
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 15:07:06 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -1,20 +1,13 @@
-alert("TEST");
-var RandomFlix = new function()
-{
-	this.run = function()
-	{
-		alert("Run bookmarklet");
-		//alert("Number Of Seasons: " + $("#seasonsNav li").length)
-		//alert("Number Of Episodes In This Season: " + $(".episodeList li").length);
-		var season_count = $("#seasonsNav li").length;
-		
-		//$.when(
-			$("#seasonsNav li").eq(season_count - 1).triggerHandler("click");
-		//).done(function()
-		//{
-		//	var episode_count = $(".episodeList li").length;
-		//	alert("Number of episodes: " + episode_count);
-		//	// $(".episodeList li").eq(episode_count - 1).trigger("click");
-		//});
-	}
-};
+alert("Run bookmarklet");
+//alert("Number Of Seasons: " + $("#seasonsNav li").length)
+//alert("Number Of Episodes In This Season: " + $(".episodeList li").length);
+var season_count = $("#seasonsNav li").length;
+
+//$.when(
+$("#seasonsNav li").eq(season_count - 1).triggerHandler("click");
+//).done(function()
+//{
+//	var episode_count = $(".episodeList li").length;
+//	alert("Number of episodes: " + episode_count);
+//	// $(".episodeList li").eq(episode_count - 1).trigger("click");
+//});
