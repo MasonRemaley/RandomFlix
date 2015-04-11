31bb7d67ef67fa1c37c101dfca249ff4ff6d5c77
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 15:35:51 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -1,36 +1,14 @@
 //what if implementation.js loads BEFORE jquery? won't this script then FAIL?
 
-/*var season_count = $("#seasonsNav li").length;
+var season_count = $("#seasonsNav li").length;
 alert("Seasons: " + season_count + " click last");
 
-$("#seasonsNav li").eq(season_count - 1).click(function()
-{
-  alert("Clicked");
-});
-
-//$.when(
+$.when(
   $("#seasonsNav li").eq(season_count - 1).trigger("click");
-//).done(function()
-//{
-  alert("Done clicking without 'when'");
-//});*/
-
-$.ajaxSetup({
-  global: true
+).done(function()
+{
+  var episode_count = $(".episodeList li").length;
+  alert("Episodes: " + episode_count);
 });
 
-$( document ).ajaxComplete(function() {
-  alert("Ajax complete!");
-  
-});
-
-$( document ).ajaxStart(function() {
-  alert("Ajax start!");
-  
-});
-
-$(document).ajaxError(function(e, jqxhr, settings, exception) {
-  alert(exception);
-});
-
-alert("complete/start/error + global");
+alert("when");
