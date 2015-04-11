ea7b50eaf2f51ab077a11fc01026b5d9a6eba5cb
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 16:26:11 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -4,11 +4,10 @@ alert("Attach CSS change event 1");
 var season_count = $("#seasonsNav li").length;
 alert("Seasons: " + season_count + " click last");
 
-$("#seasonsNav li").eq(season_count - 1).bind("cssClassChanged", function()
-{
-  alert("Class changed!");
-});
+var season = $("#seasonsNav li").eq(season_count - 1);
 
-$("#seasonsNav li").eq(season_count - 1).trigger("click");
+season.trigger("click");
+alert(season.hasClass("selected"));
+
 //var episode_count = $(".episodeList li").length;
 //alert("Episodes: " + episode_count);
