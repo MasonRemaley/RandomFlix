dd625ac3c8975c58324842d28aebb00def267f9b
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 17:03:21 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -1,24 +1,29 @@
 //what if implementation.js loads BEFORE jquery? won't this script then FAIL?
 
 //Get season information
-var season_count = $("#seasonsNav li").length;
-var season = $("#seasonsNav li").eq(season_count - 1);
-var first_episode_id = $(".episodeList li").eq(0).attr("data-episodeid");
-
-//Click on a season
-season.trigger("click");
-
-//Wait until the season updates
-timer = setInterval(function()
+function RandomFlix()
 {
-  if ($(".episodeList li").eq(0).attr("data-episodeid") != first_episode_id)
+  var season_count = $("#seasonsNav li").length;
+  //alert("Random Season: " + );
+  var season = $("#seasonsNav li").eq(season_count - 1);
+  var first_episode_id = $(".episodeList li").eq(0).attr("data-episodeid");
+  
+  //Click on a season
+  season.trigger("click");
+  
+  //Wait until the season updates
+  timer = setInterval(function()
   {
-    clearInterval(timer);
-    
-    //Click on the last episode
-    var episode_count = $(".episodeList li").length;
-    episode = $(".episodeList li").eq(0);
-    $(".episodeList li").eq(episode_count - 1).trigger("click");
-  }
-}, 100);
+    if ($(".episodeList li").eq(0).attr("data-episodeid") != first_episode_id)
+    {
+      clearInterval(timer);
+      
+      //Click on the last episode
+      var episode_count = $(".episodeList li").length;
+      episode = $(".episodeList li").eq(0);
+      $(".episodeList li").eq(episode_count - 1).trigger("click");
+    }
+  }, 100);
+};
 
+RandomFlix();
