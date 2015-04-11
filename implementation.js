c0d8592676bd7295bd26eb09174ede1ebdbe8e4d
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 16:45:18 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -1,30 +1,19 @@
 //what if implementation.js loads BEFORE jquery? won't this script then FAIL?
 
 var season_count = $("#seasonsNav li").length;
-//alert("Seasons: " + season_count + " click last");
-
 var season = $("#seasonsNav li").eq(season_count - 1);
 var loading = $(".ajaxLoading");
-//alert(season.hasClass("selected"));
-//alert(loading.css("display"));
+
 season.trigger("click");
-//alert(loading.css("display"));
 
 timer = setInterval(function()
 {
-  //alert(loading.css("display"));
-  
   if (loading.css != "block")
   {
     clearInterval(timer);
-    alert("Done: " + loading.css("display"));
+    
+    var episode_count = $(".episodeList li").length;
+    alert("Episodes: " + episode_count);
   }
 }, 100);
-//while (loading.css("display") == "block") {}
 
-//while (loading.css("display") != "none") {}
-
-//alert(season.hasClass("selected"));
-
-//var episode_count = $(".episodeList li").length;
-//alert("Episodes: " + episode_count);
