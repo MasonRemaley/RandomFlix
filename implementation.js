fbdb07097dfa2a6c85ec0174f4650e9e3d270a3d
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 14:38:53 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -2,10 +2,10 @@ var RandomFlix = new function()
 {
 	this.count_episodes = function()
 	{
-		alert("Number Of Seasons: " + $("#seasonsNav li").length)
-		alert("Number Of Episodes In This Season: " + $(".episodeList li").length);
+		//alert("Number Of Seasons: " + $("#seasonsNav li").length)
+		//alert("Number Of Episodes In This Season: " + $(".episodeList li").length);
 		
 		var seasons = $("#seasonsNav li").length;
-		$("#seasonsNav li").eq(seasons).trigger("click");
+		$("#seasonsNav li").eq(seasons - 1).trigger("click");
 	}
 };
