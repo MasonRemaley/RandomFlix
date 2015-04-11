8506d611078d6fce232b606ceb7759c26b4a822f
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 14:44:41 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -4,6 +4,7 @@ var RandomFlix = new function()
 	{
 		//alert("Number Of Seasons: " + $("#seasonsNav li").length)
 		//alert("Number Of Episodes In This Season: " + $(".episodeList li").length);
+		alert("Play last episode of last season");
 		
 		var season_count = $("#seasonsNav li").length;
 		$("#seasonsNav li").eq(season_count - 1).trigger("click");
