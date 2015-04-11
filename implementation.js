65799c91a85a1aec30dae5d9abee8be71286924d
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 15:15:10 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -3,6 +3,11 @@
 var season_count = $("#seasonsNav li").length;
 alert("Seasons: " + season_count + " click last");
 
+$("#seasonsNav li").eq(season_count - 1).click(function()
+{
+  alert("Clicked");
+});
+
 $.when(
   $("#seasonsNav li").eq(season_count - 1).trigger("click")
 ).done(function()
