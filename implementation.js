ef9c3da363ed8b0c9a46a3d4161e758cef27c57c
Mason Remaley <MasonRemaley@users.noreply.github.com>
2015-04-11 15:10:39 -0400

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 var season_count = $("#seasonsNav li").length;
-alert("Seasons: " + season_count);
-
-$("#seasonsNav li").eq(season_count - 1).triggerHandler("click");
+alert("Seasons: " + season_count + " click last");
+$("#seasonsNav li").eq(season_count - 1).trigger("click");
+alert("Done clicking");
 //	var episode_count = $(".episodeList li").length;
 //	// $(".episodeList li").eq(episode_count - 1).trigger("click");
