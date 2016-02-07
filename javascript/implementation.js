69e350f6cfae25c7ce3a6adc2182ab1e409ee83f
Mason Remaley <MasonRemaley@users.noreply.github.com>
2016-02-07 02:37:24 -0500

    Update implementation.js

--- javascript/implementation.js
+++ javascript/implementation.js
@@ -8,9 +8,9 @@ var RandomFlix = new function()
     console.log("main()");
     
     //Load JQuery
-    /*var jquery = document.createElement("RandomFlix_jquery");
-    jquery.type = "text/javascript";
-    jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";*/
+    //var jquery = document.createElement("RandomFlix_jquery");
+    //jquery.type = "text/javascript";
+    //jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
   
     document.getElementsByTagName("head")[0].appendChild(jquery);
     
